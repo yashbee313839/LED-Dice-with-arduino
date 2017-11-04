dc5b07d52ef825191f9bc9783844c7b350cdcc1c
Yashwanth G <yashbee313839@gmail.com>
2017-11-04 14:32:12 +0530

    Add files via upload

---
+++ Dice.ino.ino
@@ -0,0 +1,55 @@
+
+int pinLeds1 = 10;
+int pinLeds2 = 9;
+int pinLeds3 = 8;
+int buttonPin = 7;
+int buttonState;
+long ran;
+int time = 2000;
+
+void setup ()
+{
+  pinMode (pinLeds1, OUTPUT);
+  pinMode (pinLeds2, OUTPUT);
+  pinMode (pinLeds3, OUTPUT);
+  pinMode (buttonPin, INPUT);
+  randomSeed(analogRead(0));
+}
+
+void loop()
+{
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH){
+    ran = random(1, 7);
+    if (ran == 1){
+      digitalWrite (pinLeds3, HIGH);
+      delay (time);
+    }
+    if (ran == 2){
+      digitalWrite (pinLeds2, HIGH);
+      delay (time);
+    }
+    if (ran == 3){
+      digitalWrite (pinLeds3, HIGH);
+      digitalWrite (pinLeds2, HIGH);
+      delay (time);
+    }
+    if (ran == 4){
+      digitalWrite (pinLeds1, HIGH);
+      delay (time);
+    }
+    if (ran == 5){
+      digitalWrite (pinLeds1, HIGH);
+      digitalWrite (pinLeds3, HIGH);
+      delay (time);
+   }
+   if (ran == 6){
+      digitalWrite (pinLeds1, HIGH);
+      digitalWrite (pinLeds2, HIGH);
+      delay (time);
+   }
+  }
+  digitalWrite (pinLeds1, LOW);
+  digitalWrite (pinLeds2, LOW);
+  digitalWrite (pinLeds3, LOW);
+}
